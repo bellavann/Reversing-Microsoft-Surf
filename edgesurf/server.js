9875023e235978c01de13d9d2c6363f64a6efa36
bellavann <108235424+bellavann@users.noreply.github.com>
2024-04-24 12:50:54 -0400

    Code needed to load Reverse Engineered Version

---
+++ edgesurf/server.js
@@ -0,0 +1,51 @@
+/**
+ * Javascript file to allow for creation of server using Node.js.
+ */
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  const filePath = path.join(__dirname, req.url);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('404 Not Found');
+    } else {
+      const contentType = getContentType(filePath);
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+});
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case '.html':
+      return 'text/html';
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'text/javascript';
+    case '.png':
+      return 'image/png';
+    // Add more cases for other file types as needed
+    default:
+      return 'text/plain';
+  }
+}
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
